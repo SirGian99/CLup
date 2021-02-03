3e8d5cb8a0862212e28e41901b5f4b6a78f73bb2
Emanuele <emanueletriuzzi@gmail.com>
2021-02-03 14:53:05 +0100

    updates on StoreInfoInt & StoreStatushandler

--- Implementation/JavaServer/CLup/REST-API/src/main/java/StoreInfoInt.java
+++ Implementation/JavaServer/CLup/REST-API/src/main/java/StoreInfoInt.java
@@ -1,14 +1,10 @@
 import javax.ejb.EJB;
 import javax.ws.rs.*;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 
 import it.polimi.se2.ricciosorrentinotriuzzi.Store;
 
 import it.polimi.se2.ricciosorrentinotriuzzi.*;
 import it.polimi.se2.ricciosorrentinotriuzzi.components.DataModel;
-import org.json.JSONArray;
-import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -17,74 +13,34 @@ import java.util.Set;
 
 @Path("/StoreInfoInt/")
 public class StoreInfoInt {
-    @EJB(name = "it.polimi.se2.ricciosorrentinotriuzzi.components/DataModel")
-    private DataModel dataModel;
+    @EJB(name = "it.polimi.se2.ricciosorrentinotriuzzi.StoreStatusHandler")
+    private StoreStatusHandler ssh;
 
     @GET
     @Path("{store}")
     @Produces("text/plain")
     public String getStoreName(@PathParam("store") String storeID) {
-        return dataModel.getStore(storeID).getName();
+        return ssh.getStoreNameByID(storeID);
     }
-
 
     @GET
     @Path("store/{storeID}/generalInfo")
     @Produces("application/json")
     public StoreInfo getStoreGeneralInfo(@PathParam("storeID") String storeID) {
-        Chain chain = dataModel.getChainFromStore(storeID);
-        Store store = dataModel.getStore(storeID);
-        Address address = null;
-        if(store.getAddress() != null) {
-            address = dataModel.getAddress(store.getAddress().getId());
-        }
-        return new StoreInfo(chain, store, address);
+        return ssh.getStoreGeneralInfo(storeID);
     }
 
     @GET
     @Path("chainstore")
     @Produces("application/json")
     public ChainsAndAutonomousStores getChainsAndAutonomousStores(@QueryParam("city") String city) {
-        System.out.println("\n\n\n" + city);
-        List<Address> addresses = new ArrayList<>();
-        if(city != null) {
-            addresses = dataModel.getAddressesByCity(city);
-        }
-        Set<Chain> chains = new HashSet<>();
-        Set<Store> stores = new HashSet<>();
-        for (Address a: addresses) {
-            Store store = a.getStore();
-            if(store != null) {
-                Chain chain = store.getChain();
-                if (chain != null) {
-                    chains.add(chain);
-                } else {
-                    stores.add(store);
-                }
-            }
-        }
-        return new ChainsAndAutonomousStores(new ArrayList<Chain>(chains), new ArrayList<Store>(stores));
+        return ssh.getChainsAndAutonomousStores(city);
     }
 
     @GET
     @Path("chain/{name}/stores")
     @Produces("application/json")
     public Stores getChainStores(@PathParam("name") String chain, @QueryParam("city") String city) {
-        ArrayList<StoreInfo> stores = new ArrayList<>();
-        if(city != null) {
-            List<Address> addresses = dataModel.getAddressesByCity(city);
-            for (Address a: addresses) {
-                Store store = a.getStore();
-                if(store != null && store.getChain() != null)
-                    if(store.getChain().getName().equals(chain))
-                        stores.add(new StoreInfo(store.getChain(), store, a));
-            }
-        } else {
-            Chain c = dataModel.getChainByName(chain);
-            if(c != null)
-                for(Store s : c.storeList())
-                    stores.add(new StoreInfo(s.getChain(), s, s.getAddress()));
-        }
-        return new Stores(stores);
+        return ssh.getChainStores(chain, city);
     }
 }
