a6ab8fbf9d1ec2f4f762ebd025ea680b5b725d38
Emanuele <emanueletriuzzi@gmail.com>
2021-02-03 17:03:38 +0100

    Removed outdated methods and classes

--- Implementation/JavaServer/CLup/BusinessModule/src/main/java/StoreStatusHandler.java
+++ Implementation/JavaServer/CLup/BusinessModule/src/main/java/StoreStatusHandler.java
@@ -7,10 +7,7 @@ import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
-
 @Stateless
 public class StoreStatusHandler {
     @EJB(name = "it.polimi.se2.ricciosorrentinotriuzzi.components/DataModel")
@@ -21,41 +18,12 @@ public class StoreStatusHandler {
         return dataModel.getStore(storeID).getName();
     }
 
-    public StoreInfo getStoreGeneralInfo(String storeID) {
-        Chain chain = dataModel.getChainFromStore(storeID);
-        Store store = dataModel.getStore(storeID);
-        Address address = null;
-        if (store.getAddress() != null) {
-            address = dataModel.getAddress(store.getAddress().getId());
-        }
-        return new StoreInfo(chain, store, address);
-    }
-    public JSONObject getStoreGeneralInfo2(String storeID) {
+    public JSONObject getStoreGeneralInfo(String storeID) {
         Store store = dataModel.getStore(storeID);
         return store.toJson();
     }
 
-    public ChainsAndAutonomousStores getChainsAndAutonomousStores(String city) {
-        List<Address> addresses = new ArrayList<>();
-        if(city != null) {
-            addresses = dataModel.getAddressesByCity(city);
-        }
-        Set<Chain> chains = new HashSet<>();
-        Set<Store> stores = new HashSet<>();
-        for (Address a: addresses) {
-            Store store = a.getStore();
-            if(store != null) {
-                Chain chain = store.getChain();
-                if (chain != null) {
-                    chains.add(chain);
-                } else {
-                    stores.add(store);
-                }
-            }
-        }
-        return new ChainsAndAutonomousStores(new ArrayList<Chain>(chains), new ArrayList<Store>(stores));
-    }
-    public JSONObject getChainsAndAutonomousStores2(String city) {
+    public JSONObject getChainsAndAutonomousStores(String city) {
         JSONObject json = new JSONObject();
         JSONArray chains = new JSONArray();
         JSONArray stores = new JSONArray();
@@ -79,26 +47,7 @@ public class StoreStatusHandler {
         return json;
     }
 
-    public Stores getChainStores(String chain, String city) {
-        ArrayList<StoreInfo> stores = new ArrayList<>();
-        if(city != null) {
-            List<Address> addresses = dataModel.getAddressesByCity(city);
-            for (Address a: addresses) {
-                Store store = a.getStore();
-                if(store != null && store.getChain() != null)
-                    if(store.getChain().getName().equals(chain))
-                        stores.add(new StoreInfo(store.getChain(), store, a));
-            }
-        } else {
-            Chain c = dataModel.getChainByName(chain);
-            if(c != null)
-                for(Store s : c.storeList())
-                    stores.add(new StoreInfo(s.getChain(), s, s.getAddress()));
-        }
-        return new Stores(stores);
-    }
-
-    public JSONObject getChainStores2(String chain, String city) {
+    public JSONObject getChainStores(String chain, String city) {
         JSONObject json = new JSONObject();
         JSONArray stores = new JSONArray();
         if(city != null) {
@@ -117,103 +66,5 @@ public class StoreStatusHandler {
         }
          json.put("stores", stores);
         return json;
-    }
-}
-
-class StoreInfo implements Serializable {
-    public String chainName;
-    public String name;
-    public AddressJson address;
-    public String description;
-    public byte[] image;
-    public Integer currentOccupancy;
-    public Integer maximumOccupancy;
-    public Double safetyThreshold;
-    public ArrayList<DayIntervalJson> workingHours;
-    public ArrayList<ProductsectionJson> productSections;
-
-    public StoreInfo(Chain chain, Store store, Address address){
-        if (chain != null) {
-            this.chainName = chain.getName();
-        }
-        this.name = store.getName();
-        this.description = store.getDescription();
-        this.image = store.getImage();
-        this.currentOccupancy = store.getCurrentOccupancy();
-        this.maximumOccupancy = store.getMaximumOccupancy();
-        this.safetyThreshold = store.getSafetyThreshold();
-        this.address = null;
-        if (address != null) {
-            this.address = new AddressJson(address);
-        }
-        workingHours = new ArrayList<>();
-        for (Dayinterval di:store.getWorkingHours()) {
-            workingHours.add(new DayIntervalJson(di));
-        }
-        productSections = new ArrayList<>();
-        for (Productsection ps:store.getProductSections()) {
-            productSections.add(new ProductsectionJson(ps));
-        }
-    }
-}
-
-class AddressJson implements Serializable {
-    public long id;
-    public String streetName;
-    public String streetNumber;
-    public String city;
-    public String postalCode;
-    public String country;
-    public AddressJson(Address a) {
-        this.id = a.getId();
-        this.streetName = a.getStreetName();
-        this.streetNumber = a.getStreetNumber();
-        this.city = a.getCity();
-        this.postalCode = a.getPostalCode();
-        this.country = a.getCountry();
-    }
-}
-
-class ProductsectionJson implements Serializable {
-    public String name;
-    public Double currentOccupancy;
-    public Integer maximumOccupancy;
-    public ProductsectionJson(Productsection ps) {
-        this.name = ps.getName();
-        this.currentOccupancy = ps.getCurrentOccupancy();
-        this.maximumOccupancy = ps.getMaximumOccupancy();
-    }
-}
-
-class DayIntervalJson implements Serializable {
-    public long id;
-    public int dayOfTheWeek;
-    public String start;
-    public String end;
-    public DayIntervalJson(Dayinterval di) {
-        this.id = di.getId();
-        this.dayOfTheWeek = di.getDayOfTheWeek();
-        this.start = di.getStart().toString();
-        this.end = di.getEnd().toString();
-    }
-}
-
-class ChainsAndAutonomousStores implements Serializable{
-    public List<Chain> chains;
-    public List<StoreInfo> stores;
-
-    public ChainsAndAutonomousStores(ArrayList<Chain> chains, ArrayList<Store> stores) {
-        this.chains = chains;
-        this.stores = new ArrayList<>();
-        for(Store s : stores){
-            this.stores.add(new StoreInfo(s.getChain(), s, s.getAddress()));
-        }
-    }
-}
-
-class Stores implements Serializable {
-    public List<StoreInfo> stores;
-    public Stores(ArrayList<StoreInfo> stores){
-        this.stores = stores;
     }
 }
--- Implementation/JavaServer/CLup/REST-API/src/main/java/StoreInfoInt.java
+++ Implementation/JavaServer/CLup/REST-API/src/main/java/StoreInfoInt.java
@@ -20,44 +20,25 @@ public class StoreInfoInt {
     @GET
     @Path("store/{storeID}/generalInfo")
     @Produces("application/json")
-    public StoreInfo getStoreGeneralInfo(@PathParam("storeID") String storeID) {
-        return ssh.getStoreGeneralInfo(storeID);
-    }
-
-    @GET
-    @Path("store/{storeID}/generalInfo2")
-    @Produces("application/json")
-    public Response getStoreGeneralInfo2(@PathParam("storeID") String storeID) {
-        JSONObject jsonResponse = ssh.getStoreGeneralInfo2(storeID);
+    public Response getStoreGeneralInfo(@PathParam("storeID") String storeID) {
+        JSONObject jsonResponse = ssh.getStoreGeneralInfo(storeID);
         return Response.ok().entity(jsonResponse.toString()).type(MediaType.APPLICATION_JSON).build();
     }
 
     @GET
     @Path("chainstore")
     @Produces("application/json")
-    public ChainsAndAutonomousStores getChainsAndAutonomousStores(@QueryParam("city") String city) {
-        return ssh.getChainsAndAutonomousStores(city);
-    }
-    @GET
-    @Path("chainstore2")
-    @Produces("application/json")
-    public Response getChainsAndAutonomousStores2(@QueryParam("city") String city) {
-        JSONObject jsonResponse = ssh.getChainsAndAutonomousStores2(city);
+    public Response getChainsAndAutonomousStores(@QueryParam("city") String city) {
+        JSONObject jsonResponse = ssh.getChainsAndAutonomousStores(city);
         return Response.ok().entity(jsonResponse.toString()).type(MediaType.APPLICATION_JSON).build();
     }
+
 
     @GET
     @Path("chain/{name}/stores")
     @Produces("application/json")
-    public Stores getChainStores(@PathParam("name") String chain, @QueryParam("city") String city) {
-        return ssh.getChainStores(chain, city);
-    }
-
-    @GET
-    @Path("chain/{name}/stores2")
-    @Produces("application/json")
-    public Response getChainStores2(@PathParam("name") String chain, @QueryParam("city") String city) {
-        JSONObject jsonResponse = ssh.getChainStores2(chain, city);
+    public Response getChainStores(@PathParam("name") String chain, @QueryParam("city") String city) {
+        JSONObject jsonResponse = ssh.getChainStores(chain, city);
         return Response.ok().entity(jsonResponse.toString()).type(MediaType.APPLICATION_JSON).build();
     }
 }
