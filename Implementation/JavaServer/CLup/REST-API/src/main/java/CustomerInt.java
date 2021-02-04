ec709de1cf41f6529d96bf1e7d157a17acf4c77f
Giancarlo Sorrentino <giancarlosorrentino99@gmail.com>
2021-02-04 23:52:43 +0100

    Edit CustomerInt e CustomerController

--- Implementation/JavaServer/CLup/REST-API/src/main/java/CustomerInt.java
+++ Implementation/JavaServer/CLup/REST-API/src/main/java/CustomerInt.java
@@ -5,6 +5,7 @@ import org.json.JSONObject;
 import javax.ejb.EJB;
 import javax.ws.rs.*;
 import javax.ws.rs.core.*;
+import java.util.List;
 
 @Path("/customer/")
 
@@ -34,21 +35,20 @@ public class CustomerInt {
     @Path("{id}")
     @Produces("application/json")
     public Response getCustomerRequests(@PathParam("id") String customerID) {
-        Customer customer = cc.getCustomerByID(customerID);
-        if (customer == null) {
-            System.out.println("Error while getting customer requests");
-            return Response.status(Response.Status.NOT_ACCEPTABLE).build();
-        }
+        List<Booking> bookings = cc.getCustomerActiveBookings(customerID);
+        List<Lineup> lineups = cc.getCustomerActiveLineups(customerID);
+        if(bookings == null || lineups == null)
+            return Response.status(Response.Status.NOT_FOUND).build();
         System.out.println("Requests of "+customerID);
         JSONObject jsonResponse = new JSONObject();
         JSONArray toAppend = new JSONArray();
-        for(Booking b : customer.getBookings()) {
+        for(Booking b : bookings) {
             toAppend.put(b.toJson());
         }
         System.out.println("Bookings:\n"+toAppend);
         jsonResponse.put("bookingRequests", toAppend);
         toAppend = new JSONArray();
-        for(Lineup lu : customer.getLineups()) {
+        for(Lineup lu : lineups) {
             toAppend.put(lu.toJson());
         }
         System.out.println("LUR:\n"+toAppend);
